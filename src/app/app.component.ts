0777e61107443c7c10fb66201f430c1aa02ba4ee
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-01-31 19:23:56 +0530

    "nested component using template instead of templateUrl"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `<div><h1>root view</h1><app-employee></app-employee></div>`,
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-ang8-app';
+    title = 'my-ang8-app';
 
- firstvar = ' subhankari';
- lastname = 'padhi';
- number1 = 10;
- number2 = 20;
- fullname = ' two way binding';
- imgUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
-  addTwonumbers($event) {
-   console.log( 'we got addTwonumbers op' );
-  // alert('ghjgjhg');
- }
-  onSave($event) {
-    console.log('Save button is clicked!', $event);
-  }
-  onDivClick() {
-    console.log('DIV is clicked!');
-  }
+    firstvar = ' subhankari';
+    lastname = 'padhi';
+    number1 = 10;
+    number2 = 20;
+    fullname = ' two way binding';
+    imgUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    addTwonumbers($event) {
+        console.log( 'we got addTwonumbers op' );
+        // alert('ghjgjhg');
+    }
+    onSave($event) {
+        console.log('Save button is clicked!', $event);
+    }
+    onDivClick() {
+        console.log('DIV is clicked!');
+    }
 }
+/*
+'./app.component.html',*/
